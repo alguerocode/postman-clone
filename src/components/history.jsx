1194324ec1c0db7d072710868600edd68d73c573
salah alhashmi <alhashmis246@gmail.com>
2021-07-22 10:48:00 -0700

    init histroy

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const History = () => {
-    return ( 
-        <React.Fragment>
-            <h1>History table</h1>
-        </React.Fragment>
-     );
-}
- 
+  return (
+    <React.Fragment>
+      <ul className="list-group ms-5 list-group-numbered">
+        <li className="list-group-item d-flex justify-content-between align-items-center pe-2">
+            http://localhost:3000/add
+          <span className="badge bg-primary rounded-pill">post</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        http://localhost:3000/delete
+          <span className="badge bg-primary rounded-pill">delete</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        http://localhost:3000/get
+          <span className="badge bg-primary rounded-pill">get</span>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
+};
+
 export default History;
