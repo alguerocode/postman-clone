136e62bb98d90daca72b99e1a879f1abceec3d68
salah alhashmi <alhashmis246@gmail.com>
2021-07-22 09:09:46 -0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-// implementig lazy loading
-
-const Counter = React.lazy(() => import('./components/Counter'));
-
 const App = () => {
   return (
     <div className="app">
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <Counter />
-      </React.Suspense>
+      <h1>welcome</h1>
     </div>
   );
 };
