5e95d85c93d6ecafffd7b288b17b09be4363b519
salah alhashmi <alhashmis246@gmail.com>
2021-07-22 11:32:35 -0700

    done with styling

--- src/components/response-table.jsx
+++ src/components/response-table.jsx
@@ -1,80 +1,68 @@
-import React from "react";
+import React,{useState} from "react";
 
 const ResponseTable = () => {
-  return (
-    <React.Fragment>
-      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li class="nav-item" role="presentation">
-          <button
-            class="nav-link active"
-            id="pills-home-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-home"
-            type="button"
-            role="tab"
-            aria-controls="pills-home"
-            aria-selected="true"
-          >
-            Body
-          </button>
-        </li>
-        <li class="nav-item" role="presentation">
-          <button
-            class="nav-link"
-            id="pills-profile-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-profile"
-            type="button"
-            role="tab"
-            aria-controls="pills-profile"
-            aria-selected="false"
-          >
-            Profile
-          </button>
-        </li>
-        <li class="nav-item" role="presentation">
-          <button
-            class="nav-link"
-            id="pills-contact-tab"
-            data-bs-toggle="pill"
-            data-bs-target="#pills-contact"
-            type="button"
-            role="tab"
-            aria-controls="pills-contact"
-            aria-selected="false"
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-      <div class="tab-content" id="pills-tabContent">
-        <div
-          class="tab-pane fade show active"
-          id="pills-home"
-          role="tabpanel"
-          aria-labelledby="pills-home-tab"
-        >
-          ...
-        </div>
-        <div
-          class="tab-pane fade"
-          id="pills-profile"
-          role="tabpanel"
-          aria-labelledby="pills-profile-tab"
-        >
-          ...
-        </div>
-        <div
-          class="tab-pane fade"
-          id="pills-contact"
-          role="tabpanel"
-          aria-labelledby="pills-contact-tab"
-        >
-          ...
-        </div>
-      </div>
-    </React.Fragment>
-  );
+  const [body, setBody] = useState("");
+  const [headers, setHeaders] = useState("");
+
+ return (
+   <React.Fragment>
+     <div className="mb-3">
+       <ul className="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
+         <li className="nav-item" role="presentation">
+           <button className="nav-link active" id="pills-body-tab" data-bs-toggle="pill" data-bs-target="#pills-body" type="button" role="tab" aria-controls="pills-body" aria-selected="true">
+             Body
+           </button>
+         </li>
+         <li className="nav-item" role="presentation">
+           <button className="nav-link" id="pills-headers-tab" data-bs-toggle="pill" data-bs-target="#pills-headers" type="button" role="tab" aria-controls="pills-headers" aria-selected="false">
+             Headers
+           </button>
+         </li>
+         <li className="nav-item align-self-end ms-1">
+           <button className="btn btn-outline-warning">
+             Send
+           </button>
+         </li>
+       </ul>
+       <div className="tab-content" id="pills-tabContent">
+         <div
+           className="tab-pane fade show active"
+           id="pills-body"
+           role="tabpanel"
+           aria-labelledby="pills-body-tab"
+         >
+             <textarea 
+             name="body"
+             className="bg-light border border-1 rounded"
+             id="body"
+             value={body} 
+             cols="70" 
+             rows="10" 
+             onChange={(e)=>setBody(e.target.value)}>
+                 {body}
+             </textarea>
+         </div>
+         <div
+           className="tab-pane fade"
+           id="pills-headers"
+           role="tabpanel"
+           aria-labelledby="pills-headers-tab"
+         >
+            <textarea 
+            name="headers" 
+            className="bg-light border border-1 rounded"
+            value={headers} 
+            id="headers" 
+            cols="70" 
+            rows="10" 
+            onChange={(e)=>setHeaders(e.target.value)}>
+                 {headers}
+             </textarea>
+         </div>
+       </div>
+     </div> 
+   </React.Fragment>
+ );
 };
 
 export default ResponseTable;
