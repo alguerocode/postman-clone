e15ac8ac96f645b2442a79118b6fec6539db71cd
salah alhashmi <alhashmis246@gmail.com>
2021-07-23 01:13:28 -0700

    done with states

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
-import { History, ResponseTable, RequestTable,NavBar } from "./all-components";
-
-
+import { History, ResponseTable, RequestTable, NavBar } from "./all-components";
 
 const App = () => {
-  const [body,setBody] = useState("");
+  const [body, setBody] = useState("");
   const [headers, setHeaders] = useState("");
-  const [ResponseData, setResponseData] = useState("");
-  const [ResponseHeaders, setResponseHeaders] = useState("");
-  const [ResponseCookie, setResponseCookie] = useState("");
-  const [History, setHistory] = useState([]);
-  
-  const sendHanlder = () =>{
+  const [responseData, setResponseData] = useState("");
+  const [responseHeaders, setResponseHeaders] = useState("");
+  const [responseCookie, setResponseCookie] = useState("");
+  const [history, setHistory] = useState([]);
 
-  }
+  const sendHanlder = () => {};
   return (
     <React.Fragment>
       {/* navbar */}
@@ -21,12 +17,25 @@ const App = () => {
       <div className="container-lx">
         <div className="row justify-content-center g-5">
           <div className="col-4">
-            <History />
+            <History history={history} setHistory={setHistory} />
           </div>
           <div className="col">
             <div className="d-flex flex-column justify-content-between align-items-start">
-              <RequestTable />
-              <ResponseTable />
+              <RequestTable
+                body={body}
+                setBody={setBody}
+                headers={headers}
+                setHeaders={setHeaders}
+                sendHanlder={sendHanlder}
+              />
+              <ResponseTable
+                responseData={responseData}
+                setResponseData={setResponseData}
+                responseCookie={responseCookie}
+                setResponseCookie={setResponseCookie}
+                responseHeaders={responseHeaders}
+                setResponseHeaders={setResponseHeaders}
+              />
             </div>
           </div>
         </div>
